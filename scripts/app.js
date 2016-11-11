d173b3ea7ec90ada61ec653a0c4d2ba2d83a17d1
Rushabh Wadkar <rushabh6792@gmail.com>
2016-11-12 00:31:38 +0530

    UI - Login & Registration

---
+++ scripts/app.js
@@ -0,0 +1,26 @@
+(function(){
+        
+    var app = angular.module("AlphaApp", ["ngRoute"]);
+    
+    app.config(function($routeProvider){
+        $routeProvider
+        .when("/login", {
+            templateUrl: "../login.html",
+            controller: 'loginCtrl'
+        })
+        .when("/register", {
+            templateUrl: "../register.html",
+            controller: 'registerCtrl'
+        })
+        .otherwise({ redirectTo: '/login'});
+    });
+    
+    
+    app.controller("loginCtrl", function($scope){
+        $scope.header = "Login";
+    });
+    
+    app.controller("registerCtrl", function($scope){
+        $scope.header = "Register";
+    });
+})();
